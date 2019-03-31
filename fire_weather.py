daa64bb84e4e0b445ce14c6c68779d841c314ccd
Dan Potter <36908041+dapotter@users.noreply.github.com>
2019-03-31 12:43:10 -0700

    Add files via upload

--- fire_weather.py
+++ fire_weather.py
@@ -496,109 +496,129 @@ def import_csv(lon_min, lon_max, lat_min, lat_max):
 
     # df with only the columns desired. Gradients for CAPE are being dropped.
     # Current sample size for Jan 1-14 from SOMPY's point of view is 98 unique maps
-    df_chosen = df_all_synvar_grid_interp_pkl.drop(columns=['CAPE Grad X', 'CAPE Grad Y'])
+    df_chosen = df_all_synvar_grid_interp.drop(columns=['CAPE Grad X', 'CAPE Grad Y'])
 
     return df_chosen, df_all_synvar_grid_interp, df_all_csv, SYNVAR_3d, df, t, x, y, z
+
+
+''' Import pickle file, export as csv for SOM training in Julia '''
+def synvarPickleToCSV(pickle_in_filename, csv_out_filename, cols_list):
+    # What is file size of pickle vs csv?
+    pickle_dir  = '~/Documents/FWP/NARR/pickle/' + pickle_in_filename
+    csv_dir     = '~/Documents/FWP/NARR/' + csv_out_filename # Writing to FWP/NARR
+
+    # Read in pickle:
+    df = pd.read_pickle(pickle_dir)
+    df = df[cols_list[0]] # Currently exporting first element of cols_list
+    print('Imported pickle as df:\n', df.head())
+    # Export df as csv:
+    df.to_csv(csv_dir, index=True, header=True) # Includes index columns, names all columns at top of file
+    return
+
+
 
 
 ''' --- Run import_csv, synvar_plot --- '''
 # df_chosen, df_all_synvar_grid_interp, df_all_csv, SYNVAR_3d, df, t, x, y, z = import_csv(-125,-116,41,50)
 ''' ----------------------------------- '''
 
+''' --- Run synvarPickleToCSV --- '''
+df = synvarPickleToCSV('df_all_synvar_grid_interp.pkl','df_all_synvar_grid_interp.csv',['H500 Grad X'])
+''' ----------------------------- '''
 
 
-### read in all csvs from folder
-path = '..\\..\\data\\'
-path = 'C:\\Users\Dan\Downloads\SOMPY_robust_clustering-master\SOMPY_robust_clustering-master\data\\'
-all_files = glob.glob(os.path.join(path, "*.csv"))
-print('all_files:\n' ,all_files[0:10])
+# ### read in all csvs from folder
+# path = '..\\..\\data\\'
+# path = 'C:\\Users\Dan\Downloads\SOMPY_robust_clustering-master\SOMPY_robust_clustering-master\data\\'
+# all_files = glob.glob(os.path.join(path, "*.csv"))
+# print('all_files:\n' ,all_files[0:10])
 
-# concat into one df
-df_from_each_file = (pd.read_csv(f, skiprows = 31) for f in all_files)
-concatenated_df   = pd.concat(df_from_each_file, ignore_index=True)
+# # concat into one df
+# df_from_each_file = (pd.read_csv(f, skiprows = 31) for f in all_files)
+# concatenated_df   = pd.concat(df_from_each_file, ignore_index=True)
 
-print('concatenated df head:\n', concatenated_df.head)
-print('concatenated df columns:\n', concatenated_df.columns)
+# print('concatenated df head:\n', concatenated_df.head)
+# print('concatenated df columns:\n', concatenated_df.columns)
 
-# get columns Lat, Long, Mean Temp, Max Temp, Min temp, Precipitation
-data = concatenated_df[['Lat', 'Long', 'Tm', 'Tx', 'Tn', 'P']]
-data = data.apply(pd.to_numeric,  errors='coerce') # Converting to floats
-data = data.dropna(how='any')
-names = ['Latitude', "longitude", 'Monthly Median temperature (C)','Monthly Max temperature (C)', 'Monthly Min temperature (C)', 'Monthly total precipitation (mm)']
+# # get columns Lat, Long, Mean Temp, Max Temp, Min temp, Precipitation
+# data = concatenated_df[['Lat', 'Long', 'Tm', 'Tx', 'Tn', 'P']]
+# data = data.apply(pd.to_numeric,  errors='coerce') # Converting to floats
+# data = data.dropna(how='any')
+# names = ['Latitude', "longitude", 'Monthly Median temperature (C)','Monthly Max temperature (C)', 'Monthly Min temperature (C)', 'Monthly total precipitation (mm)']
 
-print(data.head())
+# print(data.head())
 
-# create the SOM network and train it. You can experiment with different normalizations and initializations
-sm = SOMFactory().build(data.values, normalization = 'var', initialization='pca', component_names=names)
-sm.train(n_job=1, verbose=False, train_rough_len=2, train_finetune_len=5)
+# # create the SOM network and train it. You can experiment with different normalizations and initializations
+# sm = SOMFactory().build(data.values, normalization = 'var', initialization='pca', component_names=names)
+# sm.train(n_job=1, verbose=False, train_rough_len=2, train_finetune_len=5)
 
-# The quantization error: average distance between each data vector and its BMU.
-# The topographic error: the proportion of all data vectors for which first and second BMUs are not adjacent units.
-topographic_error = sm.calculate_topographic_error()
-quantization_error = np.mean(sm._bmu[1])
-print ("Topographic error = %s; Quantization error = %s" % (topographic_error, quantization_error))
+# # The quantization error: average distance between each data vector and its BMU.
+# # The topographic error: the proportion of all data vectors for which first and second BMUs are not adjacent units.
+# topographic_error = sm.calculate_topographic_error()
+# quantization_error = np.mean(sm._bmu[1])
+# print ("Topographic error = %s; Quantization error = %s" % (topographic_error, quantization_error))
 
-# component planes view
-from sompy.visualization.mapview import View2D
-view2D  = View2D(10,10,"rand data",text_size=12)
-view2D.show(sm, col_sz=4, which_dim="all", denormalize=True)
+# # component planes view
+# from sompy.visualization.mapview import View2D
+# view2D  = View2D(10,10,"rand data",text_size=12)
+# view2D.show(sm, col_sz=4, which_dim="all", denormalize=True)
 
-# U-matrix plot
-from sompy.visualization.umatrix import UMatrixView
+# # U-matrix plot
+# from sompy.visualization.umatrix import UMatrixView
 
-umat  = UMatrixView(width=10,height=10,title='U-matrix')
-umat.show(sm)
-
-
-
-# do the K-means clustering on the SOM grid, sweep across k = 2 to 20
-from sompy.visualization.hitmap import HitMapView
-K = 20 # stop at this k for SSE sweep
-K_opt = 18 # optimal K already found
-[labels, km, norm_data] = sm.cluster(K,K_opt)
-hits  = HitMapView(20,20,"Clustering",text_size=12)
-a=hits.show(sm)
-
-import gmplot
-
-gmap = gmplot.GoogleMapPlotter(54.2, -124.875224, 6)
-j = 0
-for i in km.cluster_centers_:
-   gmap.marker(i[0],i[1],'red', title="Centroid " + str(j))
-   j += 1
-
-gmap.draw("centroids_map.html")
+# umat  = UMatrixView(width=10,height=10,title='U-matrix')
+# umat.show(sm)
 
 
-from bs4 import BeautifulSoup
 
-def insertapikey(fname, apikey):
-   """put the google api key in a html file"""
-   def putkey(htmltxt, apikey, apistring=None):
-       """put the apikey in the htmltxt and return soup"""
-       if not apistring:
-           apistring = "https://maps.googleapis.com/maps/api/js?key=%s&callback=initMap"
-       soup = BeautifulSoup(htmltxt, 'html.parser')
-       body = soup.body
-       src = apistring % (apikey, )
-       tscript = soup.new_tag("script", src=src, async="defer")
-       body.insert(-1, tscript)
-       return soup
-   htmltxt = open(fname, 'r').read()
-   soup = putkey(htmltxt, apikey)
-   newtxt = soup.prettify()
-   open(fname, 'w').write(newtxt)
-API_KEY= 'YOUR API KEY HERE'
-insertapikey("centroids_map.html", API_KEY)
+# # do the K-means clustering on the SOM grid, sweep across k = 2 to 20
+# from sompy.visualization.hitmap import HitMapView
+# K = 20 # stop at this k for SSE sweep
+# K_opt = 18 # optimal K already found
+# [labels, km, norm_data] = sm.cluster(K,K_opt)
+# hits  = HitMapView(20,20,"Clustering",text_size=12)
+# a=hits.show(sm)
+
+# import gmplot
+
+# gmap = gmplot.GoogleMapPlotter(54.2, -124.875224, 6)
+# j = 0
+# for i in km.cluster_centers_:
+#    gmap.marker(i[0],i[1],'red', title="Centroid " + str(j))
+#    j += 1
+
+# gmap.draw("centroids_map.html")
 
 
-gmap = gmplot.GoogleMapPlotter(54.2, -124.875224, 6)
-j = 0
-for i in km.cluster_centers_:
-   gmap.marker(i[0],i[1],'red', title="Centroid " + str(j))
-   j += 1
+# from bs4 import BeautifulSoup
 
-gmap.draw("centroids_map.html")
+# def insertapikey(fname, apikey):
+#    """put the google api key in a html file"""
+#    def putkey(htmltxt, apikey, apistring=None):
+#        """put the apikey in the htmltxt and return soup"""
+#        if not apistring:
+#            apistring = "https://maps.googleapis.com/maps/api/js?key=%s&callback=initMap"
+#        soup = BeautifulSoup(htmltxt, 'html.parser')
+#        body = soup.body
+#        src = apistring % (apikey, )
+#        tscript = soup.new_tag("script", src=src, async="defer")
+#        body.insert(-1, tscript)
+#        return soup
+#    htmltxt = open(fname, 'r').read()
+#    soup = putkey(htmltxt, apikey)
+#    newtxt = soup.prettify()
+#    open(fname, 'w').write(newtxt)
+# API_KEY= 'YOUR API KEY HERE'
+# insertapikey("centroids_map.html", API_KEY)
+
+
+# gmap = gmplot.GoogleMapPlotter(54.2, -124.875224, 6)
+# j = 0
+# for i in km.cluster_centers_:
+#    gmap.marker(i[0],i[1],'red', title="Centroid " + str(j))
+#    j += 1
+
+# gmap.draw("centroids_map.html")
 
 
 
--- fire_weather_som.jl
+++ fire_weather_som.jl
@@ -2,16 +2,49 @@ using SOM
 using RDatasets
 using CSV
 
-print("--------------------------------------------------------\n\n\n\n\n\n\n")
-"""Import fire weather data from csv:"""
-H500 = CSV.read("//home//dp//Documents//FWP//NARR//All_H500_som.csv")
-print("H500[1:100,:] imported from csv:\n", H500[1:100,:])
+# print("----------------------------------------------\n\n\n\n\n\n\n")
+# """Import fire weather data from csv:"""
+# H500 = CSV.read("//home//dp//Documents//FWP//NARR//All_H500_som.csv")
+# print("H500[1:100,:] imported from csv:\n", H500[1:100,:])
+#
+# train = H500[:,2:4] # All columns: lon, lat, H500
+# print("size of train:\n", size(train))
+# print("typeof(train):\n", typeof(train))
+#
+# som = initSOM(train, 62, 62)
+#
+# # Initial training:
+# som = trainSOM(som, train, 10000)
+# # Finalise training with additional round with smaller radius:
+# som = trainSOM(som, train, 10000, r = 3.0)
+#
+# # Make X,Y vector of SOM map locations for best matching units
+# # and return their row numbers in the train data
+# winners = mapToSOM(som, train[:, :])
+# print("winners first 100 rows:\n", winners[1:100,:])
+# # Why is the output different every time? Because each initialization
+# # of the SOM is different.
+#
+# """ Plotting """
+# # Density plot of number of traning samples at each neuron:
+# plotDensity(som, fileName="som_density_H500.png")
+# # Classes plot shows class labels of training samples (uses)
+# # iris data which still has class labels in it:
+# #print("som:\n", som)
+# # freqs = classFrequencies(som, iris, :Species)
+# # print("freqs:\n", freqs)
+# # plotClasses(som, freqs, fileName="iris_som_classes.png")
 
-train = H500[:,2:4] # All columns: lon, lat, H500
+"""Import H500 x-gradient data from csv:"""
+all_synvar = CSV.read("//home//dp//Documents//FWP//NARR//df_all_synvar_grid_interp.csv")
+H500_x_grad = all_synvar[:,:] # Columns lon, lat, H500 Grad X (skip time column)
+print("H500_x_grad[1:100,:] imported from csv:\n", H500_x_grad[1:100,:])
+
+train = H500_x_grad[:,[1,2,4]] # All columns: lon, lat, H500 Grad X
 print("size of train:\n", size(train))
 print("typeof(train):\n", typeof(train))
 
-som = initSOM(train, 62, 62)
+som = initSOM(train, 31, 31)
 
 # Initial training:
 som = trainSOM(som, train, 10000)
@@ -27,7 +60,7 @@ print("winners first 100 rows:\n", winners[1:100,:])
 
 """ Plotting """
 # Density plot of number of traning samples at each neuron:
-plotDensity(som, fileName="som_density_H500.png")
+plotDensity(som, fileName="som_density_H500_x_grad.png")
 # Classes plot shows class labels of training samples (uses)
 # iris data which still has class labels in it:
 #print("som:\n", som)
