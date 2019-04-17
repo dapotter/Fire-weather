caf24764a4334540c3c9ddde87c0c8bc8d58d7f1
Dan Potter <36908041+dapotter@users.noreply.github.com>
2019-04-16 22:27:35 -0700

    Add files via upload
    Add SOM processing of NARR ERC data

--- Julia/fire_weather_som.jl
+++ Julia/fire_weather_som.jl
@@ -35,17 +35,23 @@ using CSV
 # # print("freqs:\n", freqs)
 # # plotClasses(som, freqs, fileName="iris_som_classes.png")
 
-"""Import H500 x-gradient data from csv:"""
-all_synvar = CSV.read("//home//dp//Documents//FWP//NARR//df_all_synvar_grid_interp.csv")
-H500_x_grad = all_synvar[:,:] # Columns lon, lat, H500 Grad X (skip time column)
-print("H500_x_grad[1:100,:] imported from csv:\n", H500_x_grad[1:100,:])
+"""Import data from csv:"""
+#all_synvar = CSV.read("//home//dp//Documents//FWP//NARR//df_all_synvar_grid_interp.csv")
+#H500_x_grad = all_synvar[:,:] # Columns lon, lat, H500 Grad X (skip time column)
+#print("H500_x_grad[1:100,:] imported from csv:\n", H500_x_grad[1:100,:])
 
-train = H500_x_grad[1:500000,[1,2,4]] # All columns: lon, lat, H500 Grad X
-print("train[1:100,:]:\n", train[1:100,:])
+NARR_ERC = CSV.read("//home//dp//Documents//FWP//NARR//df_NARR_ERC.csv")
+print("NARR_ERC[1:100,:] imported from csv:\n", NARR_ERC[1:100,:])
+NARR_ERC = deletecols!(NARR_ERC, :time) # Removing time column
+# train = H500_x_grad[1:500000,[1,2,4]] # All columns: lon, lat, H500 Grad X
+train = NARR_ERC[:,1:5] # All columns except for the ERC column
+print("train[1:20,:]:\n", train[1:20,:])
 print("size of train:\n", size(train))
 
-som = initSOM(train, 70, 70)
-
+# Initialize:
+hexgrid_x = 40
+hexgrid_y = 40
+som = initSOM(train, hexgrid_x, hexgrid_y)
 # Initial training:
 som = trainSOM(som, train, 10000)
 # Finalise training with additional round with smaller radius:
@@ -60,10 +66,13 @@ print("winners first 100 rows:\n", winners[1:100,:])
 
 """ Plotting """
 # Density plot of number of traning samples at each neuron:
-plotDensity(som, fileName="som_density_H500_x_grad.png")
-# Classes plot shows class labels of training samples (uses)
-# iris data which still has class labels in it:
-#print("som:\n", som)
-# freqs = classFrequencies(som, iris, :Species)
-# print("freqs:\n", freqs)
-# plotClasses(som, freqs, fileName="iris_som_classes.png")
+#plotDensity(som, fileName="som_density_H500_x_grad.png")
+f = "//home//dp//Documents//FWP//Julia//NARR_ERC_SOM_density_"*string(hexgrid_x)*"x"*string(hexgrid_y)*".png"
+plotDensity(som, fileName=f)
+
+# NARR ERC data class labels in it:
+print("som:\n", som)
+freqs = classFrequencies(som, NARR_ERC, :ERC)
+print("freqs:\n", freqs)
+f = "//home//dp//Documents//FWP//Julia//NARR_ERC_SOM_classes_"*string(hexgrid_x)*"x"*string(hexgrid_y)*".png"
+plotClasses(som, freqs, fileName=f)
