536bb0114628d08e67ea6f7675f87b5ebd6682bf
Dan Potter <36908041+dapotter@users.noreply.github.com>
2019-05-06 13:34:32 -0700

    Add files via upload
    Add monthly processing capability

--- Julia/fire_weather_som.jl
+++ Julia/fire_weather_som.jl
@@ -1,52 +1,39 @@
 using SOM
 using RDatasets
 using CSV
+using Dates
 
-function fire_weather_som(hexgrid_x, hexgrid_y)
-    # print("----------------------------------------------\n\n\n\n\n\n\n")
-    # """Import fire weather data from csv:"""
-    # H500 = CSV.read("//home//dp//Documents//FWP//NARR//All_H500_som.csv")
-    # print("H500[1:100,:] imported from csv:\n", H500[1:100,:])
-    #
-    # train = H500[:,2:4] # All columns: lon, lat, H500
-    # print("size of train:\n", size(train))
-    # print("typeof(train):\n", typeof(train))
-    #
-    # som = initSOM(train, 62, 62)
-    #
-    # # Initial training:
-    # som = trainSOM(som, train, 10000)
-    # # Finalise training with additional round with smaller radius:
-    # som = trainSOM(som, train, 10000, r = 3.0)
-    #
-    # # Make X,Y vector of SOM map locations for best matching units
-    # # and return their row numbers in the train data
-    # winners = mapToSOM(som, train[:, :])
-    # print("winners first 100 rows:\n", winners[1:100,:])
-    # # Why is the output different every time? Because each initialization
-    # # of the SOM is different.
-    #
-    # """ Plotting """
-    # # Density plot of number of traning samples at each neuron:
-    # plotDensity(som, fileName="som_density_H500.png")
-    # # Classes plot shows class labels of training samples (uses)
-    # # iris data which still has class labels in it:
-    # #print("som:\n", som)
-    # # freqs = classFrequencies(som, iris, :Species)
-    # # print("freqs:\n", freqs)
-    # # plotClasses(som, freqs, fileName="iris_som_classes.png")
-
+function fire_weather_som(hexgrid_x, hexgrid_y, NARR_gridMET_csv_in)
     """Import data from csv:"""
     #all_synvar = CSV.read("//home//dp//Documents//FWP//NARR//df_all_synvar_grid_interp.csv")
     #H500_x_grad = all_synvar[:,:] # Columns lon, lat, H500 Grad X (skip time column)
     #print("H500_x_grad[1:100,:] imported from csv:\n", H500_x_grad[1:100,:])
+    NARR_ERC = CSV.read(NARR_gridMET_csv_in)
 
-    NARR_ERC = CSV.read("//home//dp//Documents//FWP//NARR//df_NARR_ERC_categorical.csv")
+    names!(NARR_ERC, Symbol.(["lat","lon","time","h500_grad_x","h500_grad_y","pmsl_grad_x","pmsl_grad_y","cape","ERC"]) )
+    NARR_ERC = deletecols!(NARR_ERC, :lat)
+    NARR_ERC = deletecols!(NARR_ERC, :lon)
     print("NARR_ERC[1:100,:] imported from csv:\n", NARR_ERC[1:100,:])
+    # df = NARR_ERC[(NARR_ERC[:time].>=Date(1979,7,1))&(NARR_ERC[:time].<=Date(1979,7,31)),:]
+    start_date = Dates.Date("1979-08-01","y-m-d")
+    end_date = Dates.Date("1979-08-31","y-m-d")
+    df_start = NARR_ERC[(NARR_ERC[:time].>=start_date), :]
+    print("df_start[1:100,:]:\n", df_start[1:100,:])
+    df_end = NARR_ERC[NARR_ERC[:time].>=end_date, :]
+    print("df_end[1:100,:],:", df_end[1:100,:])
+    NARR_ERC = NARR_ERC[.&(NARR_ERC[:time] .>= start_date, NARR_ERC[:time] .<= end_date), :]
+    print("NARR_ERC[1:100,:] after time selection July 1 - 31, 1979:\n", NARR_ERC[1:100,:])
 
-    NARR_ERC = deletecols!(NARR_ERC, :time) # Removing time column
+    # Renaming columns:
+    NARR_ERC = deletecols!(NARR_ERC, :time)
+    # NARR_ERC = deletecols!(NARR_ERC, :time) # Removing time column
+    # NARR_ERC = deletecols!(NARR_ERC, :h500_grad_x)
+    # NARR_ERC = deletecols!(NARR_ERC, :pmsl_grad_x)
+    # NARR_ERC = deletecols!(NARR_ERC, :cape)
+    print("NARR_ERC[1:100,:] after deleting columns:\n", NARR_ERC[1:100,:])
+
     # train = H500_x_grad[1:500000,[1,2,4]] # All columns: lon, lat, H500 Grad X
-    train = NARR_ERC[:,1:7] # All columns except for the ERC column at the end
+    train = NARR_ERC[:,1:5] # All columns except for the ERC column at the end
     print("train[1:20,:]:\n", train[1:20,:])
     print("size of train:\n", size(train))
 
@@ -74,9 +61,15 @@ function fire_weather_som(hexgrid_x, hexgrid_y)
     print("som:\n", som)
     freqs = classFrequencies(som, NARR_ERC, :ERC)
     print("freqs:\n", freqs)
+    color_dict = Dict("low"=>"green","moderate"=>"yellowgreen","high"=>"yellow","very high"=>"orange","extreme"=>"red")
     f = "//home//dp//Documents//FWP//Julia//NARR_ERC_SOM_classes_"*string(hexgrid_x)*"x"*string(hexgrid_y)*".png"
-    plotClasses(som, freqs, device=:png, fileName=f)
+    plotClasses(som, freqs, device=:png, colors=color_dict, fileName=f)
 
 end
 
-fire_weather_som(60,60) # hexgrid_x, hexgrid_y
+# -----------------------------------------------------
+# Run Fire Weather SOM on categorical ERC data:
+hexgrid_x = 30
+hexgrid_y = 30
+NARR_gridMET_csv_in = "//home//dp//Documents//FWP//NARR_gridMET//csv//df_NARR_ERC_categorical.csv"
+fire_weather_som(hexgrid_x,hexgrid_y,NARR_gridMET_csv_in) # hexgrid_x, hexgrid_y
