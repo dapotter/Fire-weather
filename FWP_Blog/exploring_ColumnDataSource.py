c0fc47e25a083af50a9e082037c824139245ecf9
Dan Potter <36908041+dapotter@users.noreply.github.com>
2019-05-18 23:15:30 -0700

    Add files via upload
    Add slider and select dropdown to time series. Slider is resetting to 'h500' after 'h500_grad_x' is selected.

--- FWP_Blog/exploring_ColumnDataSource.py
+++ FWP_Blog/exploring_ColumnDataSource.py
@@ -13,6 +13,8 @@ from bokeh.sampledata.sea_surface_temperature import sea_surface_temperature
 import psycopg2
 
 import pandas as pd
+import numpy as np
+import random
 
 app = Flask(__name__)
 
@@ -46,7 +48,7 @@ conn = connect_to_db()
 # QUERY: TIME SERIES OF H500, ERC AT ONE LOCATION
 # Reading data into a list object 'results' directly from postgres fire_weather_db:
 cur = conn.cursor()
-sql =  'select id, lat, lon, date, h500, erc from narr_erc \
+sql =  'select id, lat, lon, date, h500, h500_grad_x, erc from narr_erc \
       where lat = 39.2549 and lon = 236.314 \
       order by id'
 df = pd.read_sql(sql, conn)
@@ -55,13 +57,44 @@ conn.close()
 
 print('###################################################')
 df.set_index('date', inplace=True)
-df = df[['h500']]
+df = df[['h500_grad_x']]
 source = ColumnDataSource(df)
 print('df:\n', df)
 print('source:\n', source)
 
 data = df.rolling(5).mean()
-print('data:\n', data)
+print('h500_grad_x rolling mean data:\n', data)
 source.data = ColumnDataSource(data=data).data
-print('source.data:\n', source.data)
+print('h500_grad_x rolling mean source.data:\n', source.data)
 
+data = df[['h500_grad_x']]
+print('h500_grad_x data:\n', data)
+
+# Adding dictionary data to source:
+source.data = ColumnDataSource(data=data).data
+print('h500_grad_x source.data:\n', source.data)
+
+
+# list_type = ['All', 'Compliment', 'Sport', 'Remaining', 'Finance', 'Infrastructure', 'Complaint', 'Authority',
+#  'Danger', 'Health', 'English']
+
+
+
+# df = pd.concat([pd.DataFrame({'Subject' : [list_type[i] for t in range(110)], \
+#                    'Polarity' : [random.random() for t in range(110)], \
+#                    'Subjectivity' : [random.random() for t in range(110)]}) for i in range(len(list_type))], axis=0)
+
+# print('df:\n', df.to_string())
+
+print('data.index.values.tolist():\n', data.index.values.tolist())
+
+print('source.data:\n', source.data)
+print('Making a dataframe from source.data:\n')
+# NOTE: BUILDING A DATAFRAME FROM DICTIONARY DATA AND NAMING columns INCORRECTLY
+# INSIDE pd.DataFrame RESULTS IN nan VALUES POPULATING THE COLUMN. pd.DataFrame
+# EXPECTS COLUMN NAMES TO MATCH DICTIONARY KEYS
+# e.g. df_from_source = pd.DataFrame(data=source.data, columns=['date','y']) results
+# in a y column full of nans.
+df_from_source = pd.DataFrame(data=source.data)
+df_from_source.set_index('date', inplace=True)
+print('df_from_source:\n', df_from_source)
